6a202f2b462bce374c2bb19830172ec6212fc184
3FanYu <eric93504@gmail.com>
2023-02-28 19:51:02 +0800

    feat: add expiration index and sort index for short_url_mapping

--- app/models/short_url_mapping.rb
+++ app/models/short_url_mapping.rb
@@ -2,6 +2,9 @@ class ShortUrlMapping
   include Mongoid::Document
   include Mongoid::Timestamps
 
+  index({created_at: 1}, {expire_after_seconds: 10})
+  index({key: 1}, { unique: true})
+
   field :key, type: Integer
   field :short_url, type: String
   field :original_url, type: String
