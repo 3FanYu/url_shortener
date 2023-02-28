ed34bd5178a531dde3e3422248c06dcaf04dfc89
3FanYu <eric93504@gmail.com>
2023-02-28 19:50:17 +0800

    feat: add random number into unique key generation

--- app/helpers/short_url_helper.rb
+++ app/helpers/short_url_helper.rb
@@ -1,9 +1,14 @@
 module ShortUrlHelper
   # salt is timestamp of 2023/1/1 00:00:00 UTC
   KEY_SALT = 1672502400000
+  MAX_RANDOM_NUMBER = 10000
 
   def generate_unique_key
-    (Time.now.to_f * 1000).to_i - KEY_SALT
+    timestamp = (Time.now.to_f * 1000).to_i
+    # counter should be a global counter for in large distributed system to ensure uniqueness
+    # for this project I will just use a random number first
+    counter = rand(MAX_RANDOM_NUMBER)
+    (timestamp - KEY_SALT) * MAX_RANDOM_NUMBER + counter
   end
 
   def generate_short_url(key:)
