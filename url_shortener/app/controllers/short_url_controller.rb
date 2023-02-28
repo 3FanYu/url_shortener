01333e0ea5cfc6049c1385c880311b1870d1a29f
3FanYu <eric93504@gmail.com>
2023-03-01 00:03:23 +0800

    feat: add param validation for api

--- url_shortener/app/controllers/short_url_controller.rb
+++ url_shortener/app/controllers/short_url_controller.rb
@@ -1,5 +1,6 @@
 class ShortUrlController < ApplicationController
   def create
+    return render json: { error: 'Invalid parameter format' }, status: :unprocessable_entity unless params[:url].is_a?(String)
     render json: ::CreateShortUrlMappingService.new(url: params[:url]).run
   end
 end
